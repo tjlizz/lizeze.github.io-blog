a1525b12729fd0a4a1a8a73ae2da12bb742bc319
zeze.li <franklzz@foxmail.com>
2022-06-09 09:27:24 +0800

    修改发布脚本

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
- cd  /code/my-blog
- git checkout .
+cd  /code/my-blog
+git checkout .
 git pull
-
 npm run  build
-
 rm -rf /usr/share/nginx/html/blog/*
-
 cp -r ./public/* /usr/share/nginx/html/blog/
